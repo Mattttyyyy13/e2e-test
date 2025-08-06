ca4a2665c7659bbaa9add6f4539e9893e565d27d
Mattttyyyy13 <mblasco@salsify.com>
2025-08-07 00:59:00 +0800

    TD-1897: Sanity Check Failing a test case

--- projects/skulibrary/tests/a-sanity-check.spec.ts
+++ projects/skulibrary/tests/a-sanity-check.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('Sanity Check: Has Title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
+test('Sanity Check: Get Started Link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
@@ -15,4 +15,12 @@ test('get started link', async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
+
+test('Sanity Check: A Test Case that should fail', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Intentionally failing: Expect title to contain WRONG text
+  await expect(page).toHaveTitle(/ThisShouldFail/);  // ❌ This will fail
 });
